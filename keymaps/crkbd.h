33d161338982997210b69be80d288ba81643c3e4
ridignintraffic <michael.j.fettis@gmail.com>
2022-11-08 11:04:53 -0600

    reverting the esc keycode

--- keymaps/crkbd.h
+++ keymaps/crkbd.h
@@ -1,7 +1,7 @@
 #define LAYOUT_wrapper_split_3x6_3(...) LAYOUT_split_3x6_3(__VA_ARGS__)
 
 #define BASE \
-QK_GESC,   _________________QWERTY_L1_________________,              _________________QWERTY_R1_________________,    KC_MINS, \
+KC_GESC,   _________________QWERTY_L1_________________,              _________________QWERTY_R1_________________,    KC_MINS, \
 KC_TAB,    _________________QWERTY_L2_________________,              _________________QWERTY_R2_________________, KC_QUOT, \
 KC_LSFT,   _________________QWERTY_L3_________________,              _________________QWERTY_R3_________________,  KC_BSLS, \
                                 __THUMBS_3L__, __THUMBS_3R__
